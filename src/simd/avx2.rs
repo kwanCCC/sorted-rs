f83c5cd141b746cc2eb0381911b6fc194dde2b3a
0x29A <11437678+kwanCCC@users.noreply.github.com>
2023-05-22 14:52:06 +0800

    explanatory documents on marker trait

--- src/simd/avx2.rs
+++ src/simd/avx2.rs
@@ -1,3 +1,5 @@
+#![allow(non_camel_case_types)]
+
 ///
 ///   AVX2 impls
 #[cfg(all(
--- src/simd/mod.rs
+++ src/simd/mod.rs
@@ -1,7 +1,9 @@
+/// sse part
 #[cfg(feature = "use-avx2")]
 pub use avx2::is_sorted_avx2 as is_sorted;
 #[cfg(feature = "use-avx2")]
 pub use avx2::is_sorted_avx2_unroll4 as is_sorted_unroll4;
+/// avx part
 #[cfg(feature = "use-sse")]
 pub use sse::is_sorted_sse as is_sorted;
 #[cfg(feature = "use-sse")]
@@ -10,16 +12,21 @@ pub use sse::is_sorted_sse_unroll4 as is_sorted_unroll4;
 mod avx2;
 mod sse;
 
+/// marker trait for inputs limitation
 pub trait SinglePrecision: Sized {}
+/// u32 is valid
 impl SinglePrecision for u32 {}
+/// i32 is valid
 impl SinglePrecision for i32 {}
 
+/// `Trend` is used to describe the trending of input which u want to check
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub enum Trend {
     Ascending,
     Descending,
 }
 
+/// `is_sorted_scalar` a generic impls `is_sorted`
 #[inline]
 fn is_sorted_scalar<N: num::Integer, T: AsRef<[N]>>(
     a: T,
